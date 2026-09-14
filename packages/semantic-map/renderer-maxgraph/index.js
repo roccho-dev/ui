c4366027637b4c3372f96392c6bf7547eec23221
roccho <40359643+roccho-dev@users.noreply.github.com>
2026-09-14 16:58:46 +0900

    fix: make shared authoring the maxGraph public boundary

--- packages/semantic-map/renderer-maxgraph/index.js
+++ packages/semantic-map/renderer-maxgraph/index.js
@@ -1,2 +1,5 @@
-export { createMaxGraphAdapter } from './adapter.js';
+export {
+  createSemanticAuthoring,
+  createSemanticAuthoring as createMaxGraphAdapter,
+} from './authoring/index.js';
 export { DEFAULT_THEME } from './theme.js';
