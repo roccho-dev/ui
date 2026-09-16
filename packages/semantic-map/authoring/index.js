d4a7db00fcefd4e9725011df083a172f87f7d615
roccho <40359643+roccho-dev@users.noreply.github.com>
2026-09-16 10:29:31 +0900

    fix: provide generic data transport to semantic authoring

--- packages/semantic-map/authoring/index.js
+++ packages/semantic-map/authoring/index.js
@@ -1,1 +1,4 @@
-import './entry.js';
+import { createDataTransport } from '../../url-module/src/data-transport.mjs';
+
+globalThis.semanticMapDataTransport = createDataTransport(globalThis);
+await import('./entry.js');
